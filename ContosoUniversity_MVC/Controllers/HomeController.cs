03436c6f679bd5c1df3781b4ff68cec123a7712c
Michael Marango <mike.marango@gmail.com>
2018-02-17 03:55:39 -0800

    created about page showing stats

--- ContosoUniversity_MVC/Controllers/HomeController.cs
+++ ContosoUniversity_MVC/Controllers/HomeController.cs
@@ -5,21 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity_MVC.Models;
+using ContosoUniversity_MVC.Data;
+using ContosoUniversity_MVC.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext context;
+
+        public HomeController(SchoolContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
+            var data = from s in context.Students
+                       group s by s.EnrollmentDate into dateGroup
+                       select new EnrollmentDateGroup()
+                       {
+                           EnrollmentDate = dateGroup.Key,
+                           StudentCount = dateGroup.Count()
+                       };
 
-            return View();
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
