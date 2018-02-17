d048a2604378db5ec5310e18aa815b56c148dd59
Michael Marango <mike.marango@gmail.com>
2018-02-17 05:28:45 -0800

    added instructor view model

---
+++ ContosoUniversity_MVC/Models/ViewModels/InstructorIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity_MVC.Models.ViewModels
+{
+    public class InstructorIndexViewModel
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
