41b9987e972c6bdae4ae95253a90e227fe906150
Michael Marango <mike.marango@gmail.com>
2018-02-17 00:14:15 -0800

    created the db context

---
+++ ContosoUniversity_MVC/Data/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using ContosoUniversity_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity_MVC.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable(nameof(Course));
+            modelBuilder.Entity<Enrollment>().ToTable(nameof(Enrollment));
+            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
